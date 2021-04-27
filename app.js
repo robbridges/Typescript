0b22f1da947ce3c8168252a7cacd529a46658536
robbridges <robbridges@users.noreply.github.com>
2021-04-26 18:12:24 -0700

    Basic usage of typescript

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Time to get started...');
+function add(n1, n2, printResult, resultPhrase) {
+    if (printResult) {
+        console.log(resultPhrase + n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 3;
+var printResult = true;
+var resultPhrase = 'Result is: ';
++console.log(add(number1, number2, printResult, resultPhrase));
--- app.ts
+++ app.ts
@@ -1,1 +1,15 @@
-console.log('Time to get started...');
+function add(n1:number, n2:number, printResult: boolean,resultPhrase: string) {
+  const result = n1 + n2;
+  if (printResult) {
+    console.log(resultPhrase + result);
+  } else {
+  return result;
+  }
+}
+
+const number1 = 5;
+const number2= 3;
+const printResult = true;
+const resultPhrase = 'Result is: ';+
+
+console.log(add(number1, number2, printResult,resultPhrase));
