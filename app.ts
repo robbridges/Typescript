3159ab9c93bd0998128175d97670286d9eb775da
robbridges <robbridges@users.noreply.github.com>
2021-04-27 16:47:29 -0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,8 +1,17 @@
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: 'Rob',
   age:30,
-  hobbies: ['Sports', 'Cooking']
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
 };
+
+person.role.push('admin');
+//person.role[1] = 10;
 
 
 console.log(person.name); 
