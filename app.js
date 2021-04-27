95e3cf895ebd09b3dfd4faaa16f5fa1148d4f5e9
robbridges <robbridges@users.noreply.github.com>
2021-04-27 16:54:54 -0700

    enums

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+var userRole;
+(function (userRole) {
+    userRole[userRole["ADMIN"] = 0] = "ADMIN";
+    userRole[userRole["READ_ONLY"] = 1] = "READ_ONLY";
+    userRole[userRole["AUTHOR"] = 2] = "AUTHOR";
+})(userRole || (userRole = {}));
+;
 var person = {
     name: 'Rob',
     age: 30,
-    hobbies: ['Sports', 'Cooking']
+    hobbies: ['Sports', 'Cooking'],
+    role: [2, 'author'],
+    userRole: userRole.ADMIN
 };
+person.role.push('admin');
+//person.role[1] = 10;
 console.log(person.name);
 var favoriteActivities;
 person.hobbies.forEach(function (hobby) { return console.log(hobby); });
