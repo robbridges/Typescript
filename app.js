97eae4a1214b876dcb11a5bee96069b100f39161
robbridges <robbridges@users.noreply.github.com>
2021-04-27 17:19:59 -0700

    strict type casting

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number') {
-        result = input1 + input2;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
     }
     else {
         result = input1.toString() + input2.toString();
     }
     return result;
+    // if (resultConversion === 'as-number') {
+    //   return +result;
+    // } else {
+    //   return result.toString();
+    // }
 }
-console.log(combine(30, 26));
-console.log(combine('Max', 'Anna'));
+console.log(combine(30, 26, 'as-number'));
+console.log(combine('30', '26', 'as-number'));
+console.log(combine('Max', 'Anna', 'as-string'));
--- app.ts
+++ app.ts
@@ -1,15 +1,23 @@
 
-function combine(input1: number | string, input2: number | string, ) {
+function combine(
+  input1: number | string, 
+  input2: number | string,
+  resultConversion: string 
+  ) {
   let result;
-  if (typeof input1 === 'number' && typeof input2 ==='number') {
-     result = input1 + input2;
+  if (typeof input1 === 'number' && typeof input2 ==='number' || resultConversion === 'as-number') {
+     result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-    return result;
-  
+  return result;
+  // if (resultConversion === 'as-number') {
+  //   return +result;
+  // } else {
+  //   return result.toString();
+  // }
 }
 
-console.log(combine(30, 26));
-
-console.log(combine('Max', 'Anna'));
+console.log(combine(30, 26, 'as-number'));
+console.log(combine('30', '26', 'as-number'));
+console.log(combine('Max', 'Anna', 'as-string'));
