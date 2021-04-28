da7fda9dff4702449b36b522275f7d2644f5ff27
robbridges <robbridges@users.noreply.github.com>
2021-04-27 17:57:05 -0700

    Function type wrap up

--- app.ts
+++ app.ts
@@ -1,16 +1,29 @@
+// basic add function just with type script to force us to get numbers as results
+
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
 
+// printing result of a number, practice making sure that a function returns void, it does not force us to always return void, just says in this instance I will not return anything
 function printResult(num: number): void {
   console.log( 'Result: ' +num );
 }
 
-printResult(add(5,12));
+// setting up function with a cb as a third parameter
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
 
+printResult(add(5,12));
+// casting combine values to a fuction
 let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
 
 
 console.log(combineValues(8,8));
+// writing out function with requirements as originated in AddandHandle.
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
