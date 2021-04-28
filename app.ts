1300ffb98409b86cbad352e857137b3fc8b758a1
robbridges <robbridges@users.noreply.github.com>
2021-04-27 17:25:04 -0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
+type numOrString = number | string;
+
 
 function combine(
-  input1: number | string, 
-  input2: number | string,
-  resultConversion: string 
+  input1: numOrString, 
+  input2: numOrString,
+  resultConversion: 'as-number' | 'as-string', 
   ) {
   let result;
   if (typeof input1 === 'number' && typeof input2 ==='number' || resultConversion === 'as-number') {
