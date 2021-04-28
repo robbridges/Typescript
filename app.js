aa6ae91b003a9882f5b42febd2d3bcd9e0abb167
robbridges <robbridges@users.noreply.github.com>
2021-04-27 17:44:29 -0700

    typescript functions

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //   return +result;
-    // } else {
-    //   return result.toString();
-    // }
+function add(n1, n2) {
+    return n1 + n2;
 }
-console.log(combine(30, 26, 'as-number'));
-console.log(combine('30', '26', 'as-number'));
-console.log(combine('Max', 'Anna', 'as-string'));
+function printResult(num) {
+    console.log('Result: ' + num);
+}
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 8));
