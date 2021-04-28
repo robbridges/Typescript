96aae08587517626dd0bcc8bcc3063c98e9a7271
robbridges <robbridges@users.noreply.github.com>
2021-04-27 17:47:14 -0700

    Update app.ts

--- app.ts
+++ app.ts
@@ -8,8 +8,9 @@ function printResult(num: number): void {
 
 printResult(add(5,12));
 
-let combineValues: Function;
+let combineValues: (a: number, b: number) => number;
 
 combineValues = add;
+
 
 console.log(combineValues(8,8));
