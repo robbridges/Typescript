86fafb0ab62df46251d8e2edab13b76b49117c90
robbridges <robbridges@users.noreply.github.com>
2021-04-29 20:03:27 -0700

    more interface work

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 "use strict";
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    greet(greeting) {
+        console.log(`${greeting} ${user1.name}`);
+    }
+}
+let user1;
+user1 = new Person('Rob', 33);
+user1.greet('sup');
--- app.ts
+++ app.ts
@@ -1,5 +1,24 @@
-interface Person {
+interface Greetable {
+  name: string;
+  
+  
+  greet(phrase: string): void;
+}
+
+class Person implements Greetable {
   name: string;
   age: number;
-  
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  greet(greeting: string) {
+    console.log(`${greeting} ${user1.name}`);
+  }
 }
+
+let user1: Person;
+
+user1 = new Person('Rob', 33);
+
+user1.greet('sup');
