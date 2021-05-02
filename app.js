a2eb1568e4f26b6988b0e8acdc196686e4b55dc5
robbridges <robbridges@users.noreply.github.com>
2021-05-01 17:43:56 -0700

    Bit more generics

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 "use strict";
 const names = ['Rob', 'Shawn'];
 //names[0].split('');
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is done!');
+    }, 2000);
+});
+promise.then(data => {
+    //data.split('');
+});
--- app.ts
+++ app.ts
@@ -1,2 +1,12 @@
 const names: Array<string> = ['Rob', 'Shawn'];
 //names[0].split('');
+
+const promise:Promise<string> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('This is done!');
+  }, 2000)
+});
+
+promise.then(data => {
+  //data.split('');
+})
