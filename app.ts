40eada88571850e74cc17a60ab1d35f8bf0a1a07
robbridges <robbridges@users.noreply.github.com>
2021-05-04 18:12:20 -0700

    Working with decorators

---
+++ app.ts
@@ -0,0 +1,18 @@
+function Logger(constructor: Function) {
+  console.log('logging');
+  console.log(constructor);
+}
+
+@Logger
+class Person1 {
+  name = "Rob";
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+
+  
+}
+
+const person1 = new Person1();
+console.log(person1);
