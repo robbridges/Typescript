e767cbacbffeecc2ddfb3785037b1d0e912ee2b2
robbridges <robbridges@users.noreply.github.com>
2021-05-04 18:36:02 -0700

    more decorator work

--- app.js
+++ app.js
@@ -7,10 +7,23 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 function Logger(logString) {
     return function (constructor) {
-        console.log('logging');
+        console.log(logString);
         console.log(constructor);
     };
 }
+function WithTemplate(template, hookId) {
+    //return function(_: Function) { // we can pass in an underscore so that typescript knows we understand there's an argument here, but we're not nessecairly using it right now
+    return function (constructor) {
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector('h1').textContent = p.name;
+        }
+    };
+}
+//@Logger('Logging - Person') // better to pass the decorator as a fuction so that we can then update what our decorator initially does. 
 let Person1 = class Person1 {
     constructor() {
         this.name = "Rob";
@@ -18,7 +31,8 @@ let Person1 = class Person1 {
     }
 };
 Person1 = __decorate([
-    Logger('Logging - Person')
+    Logger('Hey Rob'),
+    WithTemplate('<h1> My Name is Rob </h1>', 'app') // executes the fuction logging template bottom up, but at initialization logger would create first
 ], Person1);
 const person1 = new Person1();
 console.log(person1);
--- app.ts
+++ app.ts
@@ -1,12 +1,27 @@
 function Logger(logString: string) {
   return function(constructor: Function) {
-    console.log('logging');
+    console.log(logString);
     console.log(constructor);
   };
   
 }
 
-@Logger('Logging - Person')
+function WithTemplate(template: string, hookId: string) {
+  //return function(_: Function) { // we can pass in an underscore so that typescript knows we understand there's an argument here, but we're not nessecairly using it right now
+    return function(constructor: any) {
+    console.log('Rendering template');
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  }
+}
+
+//@Logger('Logging - Person') // better to pass the decorator as a fuction so that we can then update what our decorator initially does. 
+@Logger('Hey Rob')
+@WithTemplate('<h1> My Name is Rob </h1>','app') // executes the fuction logging template bottom up, but at initialization logger would create first
 class Person1 {
   name = "Rob";
 
